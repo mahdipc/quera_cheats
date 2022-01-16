f945e720beb9d67a960a64c6e6868c76b29b0ba1
mahdi molavi <mhdmlv93@gmail.com>
2022-01-16 23:50:24 +0330

    دوربین مداربسته

--- 2794.py
+++ 2794.py
@@ -1,17 +1,15 @@
 import numpy as np
 
-print("Enter your number: ")
-# a = [[int(j) for j in input().split()] for i in range(3)]
-a=[[1,2],[3,2],[1,4]]
-a= np.array(a)
-X=a[:,0]
-Y=a[:,1]
+a = [[int(j) for j in input().split()] for i in range(3)]
+a = np.array(a)
+X = a[:, 0]
+Y = a[:, 1]
 # print(Y)
-x=0
-y=0
+x = 0
+y = 0
 for target_list in a:
-   if np.count_nonzero(X==target_list[0])==1:
-       x= target_list[0]
-   if np.count_nonzero(Y==target_list[1])==1:
-       y= target_list[1]
-print(x,y)
+    if np.count_nonzero(X == target_list[0]) == 1:
+        x = target_list[0]
+    if np.count_nonzero(Y == target_list[1]) == 1:
+        y = target_list[1]
+print(x, y)
