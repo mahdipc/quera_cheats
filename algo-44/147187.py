de26d083832850d57567276ab8677ccc7c3cec72
mahdi molavi <mhdmlv93@gmail.com>
2022-06-30 19:56:14 +0430

    سینوس طبیعی

--- algo-44/147187.py
+++ algo-44/147187.py
@@ -1,12 +1,9 @@
 import numpy as np
 k = int(input())
-if k == 1:
-    print(1)
-else:
-    for c1 in range(100):
-        if 2*np.pi*c1 < np.arcsin(1/k)+2*np.pi*c1:
-            print(int(np.arcsin(1/k)+2*np.pi*c1))
-            break
-        elif np.pi-np.arcsin(1/k)+2*np.pi*c1 < np.pi+2*np.pi*c1:
-            print(int(np.pi+2*np.pi*c1))
-            break
+n = 1
+while True:
+    s = np.sin(n)
+    if 0 <= s and s <= 1/k:
+        print(n)
+        break
+    n += 1
