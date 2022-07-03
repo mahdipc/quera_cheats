811bf92276dc7e6d281a5c8f0fcdeed1ccf77258
mahdi molavi <mhdmlv93@gmail.com>
2022-07-03 14:19:38 +0430

    شماره رند

--- 91713.py
+++ 91713.py
@@ -1,2 +1,23 @@
-n=int(input())
-st=[j for j in input().split()]
+n = int(input())
+numbers = [input() for i in range(n)]
+
+
+def have_4_number_duplicate(number):
+    for n in number:
+        if number.count(n) >= 4:
+            return True
+    return False
+
+
+def sequence_value(number):
+    for i in range(6):
+        if number[i] == number[i+1] and number[i] == number[i+2]:
+            return True
+    return False
+
+
+for number in numbers:
+    if number == number[::-1] or sequence_value(number) or have_4_number_duplicate(number):
+        print("Ronde!")
+    else:
+        print("Rond Nist")
