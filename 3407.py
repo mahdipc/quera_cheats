4a72f67b4311cd34b19664fa2cc7b5208506a9fb
mahdi molavi <mhdmlv93@gmail.com>
2022-08-01 17:41:49 +0430

    بمب بازی

--- 3407.py
+++ 3407.py
@@ -1,38 +1,109 @@
-import numpy as np
-n, m = input().split()
-n, m = int(n), int(m)
+n, m = tuple(map(int, input().split()))
 k = int(input())
-board = np.zeros((n, m))
+result = []
+for i in range(n):
+    result.append([0] * m)
+
 for i in range(k):
-    x, y = input().split()
-    x, y = int(x), int(y)
-    board[x-1, y-1] = 1
-
-
-def boomb(i, j):
-    if i == 0 and j == 0:
-        return board[i+1, j] + board[i, j+1]+board[i+1, j+1]
-    if i == n-1 and j == m-1:
-        return board[i-1, j] + board[i, j-1] + board[i-1, j-1]
-    if i == 0 and j == m-1:
-        return board[i, j-1] + board[i+1, j] + board[i+1, j-1]
-    if i == n-1 and j == 0:
-        return board[i-1, j] + board[i, j+1] + board[i-1, j+1]
-    if i == 0:
-        return board[i, j-1] + board[i, j+1] + board[i+1, j]+board[i+1, j+1]+board[i+1, j-1]
-    if j == 0:
-        return board[i-1, j] + board[i, j+1] + board[i+1, j]+board[i+1, j+1]+board[i-1, j+1]
-    if i == n-1:
-        return board[i, j-1] + board[i, j+1] + board[i-1, j]+board[i-1, j+1]+board[i-1, j-1]
-    if j == m-1:
-        return board[i-1, j] + board[i, j-1] + board[i+1, j]+board[i+1, j-1]+board[i-1, j-1]
-    return board[i-1, j] + board[i, j-1] + board[i, j+1] + board[i+1, j]+board[i+1, j+1]+board[i+1, j-1]+board[i-1, j+1]+board[i-1, j-1]
-
+    x, y = tuple(map(int, input().split()))
+    result[x-1][y-1] = '*'
 
 for i in range(n):
     for j in range(m):
-        if board[i][j] == 1:
-            print('*', end=" ")
-        else:
-            print(int(boomb(i, j)), end=" ")
+        counter = 0
+        if result[i][j] != '*':
+            if i > 0 and j > 0 and i+1 < n and j+1 < m:
+                if result[i-1][j-1] == '*':
+                    counter += 1
+                if result[i-1][j] == '*':
+                    counter += 1
+                if result[i-1][j+1] == '*':
+                    counter += 1
+                if result[i][j-1] == '*':
+                    counter += 1
+                if result[i][j+1] == '*':
+                    counter += 1
+                if result[i+1][j-1] == '*':
+                    counter += 1
+                if result[i+1][j] == '*':
+                    counter += 1
+                if result[i+1][j+1] == '*':
+                    counter += 1
+            elif i == 0 and j == 0:
+                if result[i+1][j] == '*':
+                    counter += 1
+                if result[i+1][j+1] == '*':
+                    counter += 1
+                if result[i][j+1] == '*':
+                    counter += 1
+            elif i+1 == n and j == 0:
+                if result[i-1][j] == '*':
+                    counter += 1
+                if result[i-1][j+1] == '*':
+                    counter += 1
+                if result[i][j+1] == '*':
+                    counter += 1
+            elif i == 0 and j+1 == m:
+                if result[i+1][j] == '*':
+                    counter += 1
+                if result[i+1][j-1] == '*':
+                    counter += 1
+                if result[i][j-1] == '*':
+                    counter += 1
+            elif i+1 == n and j+1 == m:
+                if result[i-1][j] == '*':
+                    counter += 1
+                if result[i-1][j-1] == '*':
+                    counter += 1
+                if result[i][j-1] == '*':
+                    counter += 1
+            elif i == 0 and j < m and j > 0:
+                if result[i][j-1] == '*':
+                    counter += 1
+                if result[i][j+1] == '*':
+                    counter += 1
+                if result[i+1][j-1] == '*':
+                    counter += 1
+                if result[i+1][j] == '*':
+                    counter += 1
+                if result[i+1][j+1] == '*':
+                    counter += 1
+            elif j == 0 and i < n and i > 0:
+                if result[i-1][j] == '*':
+                    counter += 1
+                if result[i-1][j+1] == '*':
+                    counter += 1
+                if result[i+1][j] == '*':
+                    counter += 1
+                if result[i+1][j+1] == '*':
+                    counter += 1
+                if result[i][j+1] == '*':
+                    counter += 1
+            elif i+1 == n and j < m and j > 0:
+                if result[i-1][j] == '*':
+                    counter += 1
+                if result[i-1][j+1] == '*':
+                    counter += 1
+                if result[i-1][j-1] == '*':
+                    counter += 1
+                if result[i][j-1] == '*':
+                    counter += 1
+                if result[i][j+1] == '*':
+                    counter += 1
+            elif j+1 == m and i < n and i > 0:
+                if result[i-1][j] == '*':
+                    counter += 1
+                if result[i-1][j-1] == '*':
+                    counter += 1
+                if result[i][j-1] == '*':
+                    counter += 1
+                if result[i+1][j-1] == '*':
+                    counter += 1
+                if result[i+1][j] == '*':
+                    counter += 1
+            result[i][j] = counter
+
+for row in result:
+    for col in row:
+        print(col, end=' ')
     print()
