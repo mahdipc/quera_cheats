549ded74115b297c0e6dcb73282510518e964d0d
mahdi molavi <mhdmlv93@gmail.com>
2022-08-16 11:01:42 +0430

    رمزهای جان‌سخت

--- 9722.py
+++ 9722.py
@@ -1,6 +1,26 @@
-import numpy as np
-from sympy import *
+n = int(input())
 
-N=int(input())
-def PraimNumber(number):
-    res=nt.isprime(np.array([1,3,5,7,9])+number*10)
+
+def is_prime(n):
+    if n == 1:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+
+list_prime = ["2", "3", "5", "7"]
+
+if n == 1:
+    print(*list_prime, sep='\n')
+else:
+    for i in range(2, n+1):
+        for i in range(10):
+            for p in list_prime:
+                if p+str(i) not in list_prime and len(p) < n and is_prime(int(p+str(i))):
+                    list_prime.append(p+str(i))
+list_prime.sort()
+for p in list_prime:
+    if len(p) == n:
+        print(p)
