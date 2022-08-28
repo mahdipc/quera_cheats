cfc5e2052bf6e88799f802d56d8ab785c493457f
mahdi molavi <mhdmlv93@gmail.com>
2022-08-28 19:57:07 +0430

    آلفا قنطورس

---
+++ 66859.py
@@ -0,0 +1,30 @@
+import string
+digs = string.digits + string.ascii_uppercase
+
+
+def int2base(x, base):
+    if x < 0:
+        sign = -1
+    elif x == 0:
+        return digs[0]
+    else:
+        sign = 1
+
+    x *= sign
+    digits = []
+
+    while x:
+        digits.append(digs[int(x % base)])
+        x = int(x / base)
+
+    if sign < 0:
+        digits.append('-')
+
+    digits.reverse()
+
+    return ''.join(digits)
+
+
+n, b = input().split()
+n, b = int(n), int(b)
+print(int2base(n, b))
