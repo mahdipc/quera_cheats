ac331292e9fa5e90aa099664844fdf3c8255af05
mahdi molavi <mhdmlv93@gmail.com>
2022-09-05 11:14:22 +0430

    Background Color Switch

---
+++ 49606/app.js
@@ -0,0 +1,15 @@
+document.getElementById('grey').addEventListener('click', function () {
+  document.body.style.background = 'grey'
+})
+
+document.getElementById('white').addEventListener('click', function () {
+  document.body.style.background = 'white'
+})
+
+document.getElementById('blue').addEventListener('click', function () {
+  document.body.style.background = 'blue'
+})
+
+document.getElementById('yellow').addEventListener('click', function () {
+  document.body.style.background = 'yellow'
+})
