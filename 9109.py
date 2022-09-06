cc97980db52f9a4377f7697327eec5959900fa5e
mahdi molavi <mhdmlv93@gmail.com>
2022-09-06 11:49:02 +0430

    مجید و ماژیک‌هاش

--- 9109.py
+++ 9109.py
@@ -1,3 +1,11 @@
 n = input()
 s = [int(j) for j in input().split()]
-dict((x, s.count(x)) for x in set(s))
+marker = dict((x, s.count(x)) for x in set(s))
+min_marker_number = min(marker.values())
+rrr = list(marker.keys())
+rrr.sort()
+for ind in rrr:
+
+    if marker[ind] == min_marker_number:
+        print(ind)
+        break
