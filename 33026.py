6506e6b0a19dfc33d25afdd33fd1241611f96ab3
mahdi molavi <mhdmlv93@gmail.com>
2022-09-09 14:58:08 +0430

    چی‌سون؟

--- 33026.py
+++ 33026.py
@@ -1,35 +1,14 @@
+import json
+
 n = int(input())
-
-
-def dicSplit(arr, item):
-    arrs = arr.split(",")
-    for element in arrs:
-        var, id = element.split(':')
-        if var.strip() == item:
-            return int(id)
-
-
-def arrSplit(arr, item):
-    item = int(item)
-    return int(arr[1:-1].split(',')[item])
-
-
-prints = {}
-operators = {}
+l = {}
 for i in range(n):
     s = input()
-    if 'print' in s:
-        var, id = s[6:].split('[')
-        prints[var.strip()] = id[:-1].strip()
-    else:
-        var, id = s.split(':=')
-        operators[var.strip()] = id.strip()
-for p in prints:
-    o = operators[p]
-    if o[0] == '[':
-        print(arrSplit(o, prints[p]))
-    else:
-        print(dicSplit(o, prints[p]))
-
-# print(prints)
-# print(operators)
+    if s.startswith("print"):
+        nameidx = s.split(" ")[1].split("[")
+        if nameidx[1][:-1].startswith("\""):
+            print(l[nameidx[0]][nameidx[1][1:-2]])
+        else:
+            print(l[nameidx[0]][int(nameidx[1][:-1])])
+    elif s.split(" ")[1] == ':=':
+        l.update({s.split(" ")[0]: json.loads(s[len(s.split(" ")[0])+4:])})
