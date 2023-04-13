ff708409487e2789391adf13b43d342205466983
mahdi molavi <mhdmlv93@gmail.com>
2023-04-13 10:42:19 +0330

    تیم ملی نخودخوری در برره

--- 10163.py
+++ 10163.py
@@ -1,12 +1,25 @@
-abc = input().split()
-arr = [input().split() for i in range(3)]
-sum = 0
-for i in range(200):
-    num = 0
-    for j in range(3):
-        if int(arr[j][0]) <= i and i < int(arr[j][1]):
-            num += 1
-        if num == 0:
-            continue
-        sum += num*int(abc[num-1])
-print(sum)
+
+
+def group_items(items):
+    result = {}
+    for item in items:
+        if result.get(item) is None:
+            result[item] = 1
+        else:
+            result[item] = result[item] + 1
+    return result
+
+
+prices = list(map(int, input().split()))
+num_of_persons = 3
+
+times = []
+for i in range(num_of_persons):
+    begin, end = map(int, input().split())
+    times.extend(list(range(begin, end)))
+
+cost = 0
+for count in group_items(times).values():
+    cost += prices[count - 1] * count
+
+print(cost)
