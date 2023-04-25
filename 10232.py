2ad87c1e90b0c8646068caa65e84fc9a6eecd983
mahdi molavi <mhdmlv93@gmail.com>
2023-04-25 19:19:44 +0330

    باقر خسته‌ست ولی بی‌فرهنگ نه

--- 10232.py
+++ 10232.py
@@ -1,3 +1,18 @@
-st = input().split()
-n, l = int(st[0]), int(st[1])
-st = [map(int(input().split())) for i in range(n)]
+
+n, l = map(int, input().split())
+
+covered_distance = 0
+total_duration = 0
+for i in range(n):
+    distance, red_duration, green_duration = map(int, input().split())
+
+    total_duration += distance - covered_distance
+    covered_distance = distance
+
+    cycle = red_duration + green_duration
+    remain = total_duration % cycle
+    if remain < red_duration:
+        total_duration += red_duration - remain
+
+total_duration += l - covered_distance
+print(total_duration)
