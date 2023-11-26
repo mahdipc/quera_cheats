76015c31420907edfabca6e27a074b688aef7b69
mahdi molavi <mhdmlv93@gmail.com>
2023-11-26 19:29:55 +0330

    قالب بندی رشته

---
+++ 197312/ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var textReplacer = new TextReplacer("@");
+
+            // template data
+            textReplacer.AddTag("humanName", "Timmy");
+
+            string inputText = "Hello, my name is @humanName, and I love @color.";
+
+            string resultText = textReplacer.ReplaceTags(inputText);
+            Console.WriteLine(resultText);
+        }
+
+        public static bool YourAction()
+        {
+            return true;
+        }
+    }
+}
---
+++ 197312/ConsoleApp/TextReplacer.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp;
+
+public class TextReplacer
+{
+    // کالکشن خود را در این قسمت تعریف کنید
+
+    public string tagChar;
+    private Dictionary<string, string> tagReplacements;
+    public TextReplacer(string tagChar)
+    {
+        this.tagChar = tagChar;
+        tagReplacements = new Dictionary<string, string>();
+    }
+
+    public void AddTag(string tagName, string tagValue)
+    {
+        tagReplacements.Add(tagChar + tagName, tagValue);
+        // تگ را به کالکشن خود اضافه کنید
+    }
+
+    public string ReplaceTags(string text)
+    {
+        foreach (var (tagName, value) in tagReplacements)
+        {
+            text = text.Replace(tagName, value);
+        }
+        return text;
+    }
+}
