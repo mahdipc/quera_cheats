4d8724126608245b6fc6684b6ca2ca818008a078
mahdi molavi <mhdmlv93@outlook.com>
2025-08-05 11:26:17 +0330

    دو شغله

---
+++ 111990.py
@@ -0,0 +1,11 @@
+day = input()
+days = {
+    "shanbe": "perspolis",
+    "yekshanbe": "bahman",
+    "doshanbe": "perspolis",
+    "seshanbe": "bahman",
+    "chaharshanbe": "perspolis",
+    "panjshanbe": "bahman",
+    "jome": "tatil",
+}
+print(days[day])
